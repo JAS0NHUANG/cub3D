c75a7351363b444f7ddd84cd7a0d5f8c5d776def
ifeelbored <ursula771023@gmail.com>
2022-03-24 23:49:21 +0100

    rotate

--- srcs/events/ft_key_event.c
+++ srcs/events/ft_key_event.c
@@ -28,7 +28,7 @@ int	ft_move_player(int key, t_cub3d *cub3d)
 	float	player_to_y;
 
 
-	if (key == XK_w)
+	if (key == XK_W)
 	{
 		printf("player x: %f, y: %f\n", cub3d->player->pos_x, cub3d->player->pos_y);
 		player_to_x = cub3d->player->pos_x + cub3d->player->dir_x * MOVE_DIST;
@@ -43,7 +43,7 @@ int	ft_move_player(int key, t_cub3d *cub3d)
 		}
 		clean_screen(cub3d);
 	}
-	if (key == XK_s)
+	if (key == XK_S)
 	{
 		player_to_x = cub3d->player->pos_x - cub3d->player->dir_x * MOVE_DIST;
 		player_to_y = cub3d->player->pos_y - cub3d->player->dir_y * MOVE_DIST;
@@ -58,7 +58,7 @@ int	ft_move_player(int key, t_cub3d *cub3d)
 		}
 		clean_screen(cub3d);
 	}
-	if (key == XK_a)
+	if (key == XK_A)
 	{
 		player_to_x = cub3d->player->pos_x - cub3d->player->dir_y * MOVE_DIST;
 		player_to_y = cub3d->player->pos_y + cub3d->player->dir_x * MOVE_DIST;
@@ -73,7 +73,7 @@ int	ft_move_player(int key, t_cub3d *cub3d)
 			clean_screen(cub3d);
 		}
 	}
-	if (key == XK_d)
+	if (key == XK_D)
 	{
 		player_to_x = cub3d->player->pos_x + cub3d->player->dir_y * MOVE_DIST;
 		player_to_y = cub3d->player->pos_y - cub3d->player->dir_x * MOVE_DIST;
@@ -96,27 +96,26 @@ int	ft_move_player(int key, t_cub3d *cub3d)
 
 int	ft_rotate_player(t_cub3d *cub3d, int key)
 {
-	if (key == XK_Left)
+	double oldDirX;
+	double oldPlaneX;
+
+	if (key == XK_LEFT)
 	{
-		cub3d->player->angle+=0.1;
-		if (cub3d->player->angle > 2 * PI)
-			cub3d->player->angle-= 2 * PI;
-		// cub3d->player->dir_x = cos(cub3d->player->angle) * cub3d->player->pos_x;
-		// cub3d->player->dir_y = sin(cub3d->player->angle) * cub3d->player->pos_y;
-		cub3d->player->dir_x = cos(cub3d->player->angle);
-		cub3d->player->dir_y = sin(cub3d->player->angle);
-		printf("here1, x=%f, y=%f\n",cub3d->player->dir_x,  cub3d->player->dir_y);
+		oldDirX = cub3d->player->dir_x;
+		cub3d->player->dir_x = cub3d->player->dir_x * cos(0.3) - cub3d->player->dir_y * sin(0.3);
+		cub3d->player->dir_y = oldDirX * sin(0.3) + cub3d->player->dir_y * cos(0.3);
+		oldPlaneX = cub3d->player->plane_x;
+		cub3d->player->plane_x = cub3d->player->plane_x * cos(0.3) - cub3d->player->plane_y * sin(0.3);
+		cub3d->player->plane_y = oldPlaneX * sin(0.3) + cub3d->player->plane_y * cos(0.3);
 	}
-	if (key == XK_Right)
+	if (key == XK_RIGHT)
 	{
-		cub3d->player->angle-=0.1;
-		if (cub3d->player->angle < 0)
-			cub3d->player->angle+= 2 * PI;
-		cub3d->player->dir_x = cos(cub3d->player->angle);
-		cub3d->player->dir_y = sin(cub3d->player->angle);
-		// cub3d->player->dir_x = cos(cub3d->player->angle) * cub3d->player->pos_x;
-		// cub3d->player->dir_y = sin(cub3d->player->angle) * cub3d->player->pos_y;
-		printf("here2, x=%f, y=%f\n",cub3d->player->dir_x,  cub3d->player->dir_y);
+		oldDirX = cub3d->player->dir_x;
+		cub3d->player->dir_x = cub3d->player->dir_x * cos(-0.3) - cub3d->player->dir_y * sin(-0.3);
+		cub3d->player->dir_y = oldDirX * sin(-0.3) + cub3d->player->dir_y * cos(-0.3);
+		oldPlaneX = cub3d->player->plane_x;
+		cub3d->player->plane_x = cub3d->player->plane_x * cos(-0.3) - cub3d->player->plane_y * sin(-0.3);
+		cub3d->player->plane_y = oldPlaneX * sin(-0.3) + cub3d->player->plane_y * cos(-0.3);
 	}
 	clean_screen(cub3d);
 	ft_print_canvas(cub3d);
@@ -129,11 +128,11 @@ int	ft_key_event(int key, t_cub3d *cub3d)
 {
 	
 	(void)cub3d;
-	if (key == XK_Escape)
+	if (key == XK_ESCAPE)
 		ft_close(cub3d);
-	if (key == XK_a || key == XK_d || key == XK_w || key == XK_s)
+	if (key == XK_A || key == XK_D || key == XK_W || key == XK_S)
 		ft_move_player(key, cub3d);
-	if (key == XK_Left || key == XK_Right)
+	if (key == XK_LEFT || key == XK_RIGHT)
 	{
 		ft_rotate_player(cub3d, key);
 		// ft_putstr_fd("rotate: ", 1);
